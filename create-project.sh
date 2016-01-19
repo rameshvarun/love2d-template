658b24c876e36fbae12054e82201a4e1076ac18b
Varun Ramesh <varunramesh@live.com>
2016-01-18 19:35:34 -0800

    Added release script to template.

--- create-project.sh
+++ create-project.sh
@@ -21,8 +21,13 @@ read GAME_NAME
   cd $OUT_DIR
   cat conf.lua | sed -e "s:\${game_id}:$GAME_ID:g" | sed -e "s:\${game_name}:$GAME_NAME:g" > conf.lua.tmp
   mv conf.lua.tmp conf.lua
+
   cat README.md | sed -e "s:\${game_name}:$GAME_NAME:g" > README.md.tmp
   mv README.md.tmp README.md
+
+  cd tools
+  cat release.py | sed -e "s:\${game_id}:$GAME_ID:g" > release.py.tmp
+  mv release.py.tmp release.py
 )
 
 (
---
+++ template/tools/release.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import errno
+import urllib
+import zipfile
+import subprocess
+import shutil
+
+GAME_ID = "${game_id}"
+
+LOVE_VERSION = "0.10.0"
+LOVE_WIN64 = "https://bitbucket.org/rude/love/downloads/love-" + LOVE_VERSION + "-win64.zip"
+LOVE_WIN32 = "https://bitbucket.org/rude/love/downloads/love-" + LOVE_VERSION + "-win32.zip"
+LOVE_OSX = "https://bitbucket.org/rude/love/downloads/love-" + LOVE_VERSION + "-macosx-x64.zip"
+
+RELEASE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "release")
+TMP_DIR = os.path.join(RELEASE_DIR, "tmp")
+LOVE_FILE = os.path.join(RELEASE_DIR, GAME_ID + ".love")
+
+def build_lovefile():
+	print "-- Creating " + LOVE_FILE + " --"
+	with zipfile.ZipFile(LOVE_FILE, 'w') as love_zip:
+		files = subprocess.check_output(["git", "ls-files"]).splitlines()
+		for f in files: love_zip.write(f)
+	print "-- Created " + LOVE_FILE + " --"
+
+def build_osx():
+	print "-- Building for OSX 64-bit --"
+	love_zip = os.path.join(TMP_DIR, "love.zip")
+	urllib.urlretrieve(LOVE_OSX, love_zip)
+
+
+	try:
+		subprocess.check_output(['unzip', '-q', love_zip, '-d', TMP_DIR], stderr=subprocess.STDOUT)
+	except subprocess.CalledProcessError as err:
+		print '-- Unzipping failed --'
+
+	os.remove(love_zip)
+
+	app_folder = os.path.join(TMP_DIR, GAME_ID + ".app")
+	os.rename(os.path.join(TMP_DIR, "love.app"), app_folder)
+	shutil.copyfile(LOVE_FILE, os.path.join(app_folder, "Contents", "Resources", GAME_ID + ".love"))
+	shutil.copyfile(os.path.join("platform", "osx", "Info.plist"), os.path.join(app_folder, "Contents", "Info.plist"))
+
+	shutil.make_archive(os.path.join(RELEASE_DIR, GAME_ID + "-osx"), "zip", root_dir=TMP_DIR)
+
+def build_windows(use32bit):
+	print "-- Building for Windows " + ("32" if use32bit else "64") + "-bit --"
+	love_zip = os.path.join(TMP_DIR, "love.zip")
+	urllib.urlretrieve(LOVE_WIN32 if use32bit else LOVE_WIN64, love_zip)
+	with zipfile.ZipFile(love_zip, 'r') as love_zipfile: love_zipfile.extractall(TMP_DIR)
+	love_folder = os.path.join(TMP_DIR, "love-0.9.2-win32" if use32bit else "love-0.9.2-win64")
+
+	with open(os.path.join(love_folder, GAME_ID + ".exe"), 'wb') as game_exe:
+		shutil.copyfileobj(open(os.path.join(love_folder, "love.exe"), "rb"), game_exe)
+		shutil.copyfileobj(open(LOVE_FILE, "rb"), game_exe)
+
+	os.remove(os.path.join(love_folder, "love.exe"))
+	OUT_DIR = GAME_ID + "-win32" if use32bit else GAME_ID + "-win64"
+	shutil.move(love_folder, os.path.join(RELEASE_DIR, OUT_DIR))
+	shutil.make_archive(os.path.join(RELEASE_DIR, OUT_DIR), "zip", root_dir=os.path.join(RELEASE_DIR, OUT_DIR))
+	shutil.rmtree(os.path.join(RELEASE_DIR, OUT_DIR))
+
+def make_directory(path, clear):
+	# Might want to clear directory
+	if clear and os.path.isdir(path):
+		shutil.rmtree(path)
+
+	# Try to create the directory
+	try: os.makedirs(path)
+	except OSError as err:
+		# If directory already exists, continue
+		if err.errno == errno.EEXIST: pass
+		else: raise err
+
+if __name__ == "__main__":
+	if len(sys.argv) != 2:
+		print "One argument required."
+		sys.exit(1)
+
+	print "-- Release Script --"
+
+	make_directory(RELEASE_DIR, False)
+	make_directory(TMP_DIR, True)
+
+	build_lovefile()
+
+	if sys.argv[1] == "win32":
+		build_windows(True)
+	elif sys.argv[1] == "win64":
+		build_windows(False)
+	elif sys.argv[1] == "love":
+		pass
+	elif sys.argv[1] == "osx":
+		build_osx()
+	elif sys.argv[1] == "all":
+		build_windows(True)
+		build_windows(False)
+		build_osx()
+	else:
+		print "Unkown platform"
+		sys.exit(1)
+
+	shutil.rmtree(TMP_DIR)
